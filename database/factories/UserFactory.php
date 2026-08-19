565b2fd4fd8a5897da24428a734e5f88b49cb9c8
ph2708 <ph2708@outlook.com>
2026-08-19 14:17:40 -0300

    Fix UserFactory em ambiente de producao sem require-dev

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -25,9 +25,10 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'name' => 'Usuário Exemplo',
+            'email' => Str::random(8) . '@maquigeral.com.br',
             'email_verified_at' => now(),
+            'role' => 'ADMIN',
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
         ];
